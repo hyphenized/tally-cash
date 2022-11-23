bd269549d50611868c341317086315c2e99fafc2
hyphenized <28708889+hyphenized@users.noreply.github.com>
2022-11-23 16:09:37 -0500

    Add stats to NFTs Header

--- ui/pages/NFTs.tsx
+++ ui/pages/NFTs.tsx
@@ -1,15 +1,45 @@
-import { getHasAccounts } from "@tallyho/tally-background/redux-slices/selectors"
+import {
+  getHasAccounts,
+  selectMainCurrencySign,
+  selectMainCurrencySymbol,
+} from "@tallyho/tally-background/redux-slices/selectors"
+import { formatCurrencyAmount } from "@tallyho/tally-background/redux-slices/utils/asset-utils"
 import React, { ReactElement } from "react"
 import NFTsExploreBanner from "../components/NFTS_update/NFTsExploreBanner"
 import NFTsHeader from "../components/NFTS_update/NFTsHeader"
 import { useBackgroundSelector } from "../hooks"
 
+// TODO: Remove these stubs
+const stubSelectNFTCount = () => 16
+const stubSelectCollectionCount = () => 2
+const stubSelectBadgeCount = () => 5
+
 export default function NFTs(): ReactElement {
   const hasAccounts = useBackgroundSelector(getHasAccounts)
+  const nftCounts = useBackgroundSelector(stubSelectNFTCount)
+  const collectionCount = useBackgroundSelector(stubSelectCollectionCount)
+  const badgeCount = useBackgroundSelector(stubSelectBadgeCount)
+
+  const mainCurrencySign = useBackgroundSelector(selectMainCurrencySign)
+  const mainCurrencySymbol = useBackgroundSelector(selectMainCurrencySymbol)
+  const NFTsLoading = useBackgroundSelector(() => false)
+
+  // TODO: Remove these stubs
+  const someAmount = formatCurrencyAmount(mainCurrencySymbol, 240_241, 0)
+  const someAmountInETH = "21.366 ETH"
 
   return (
     <div className="page_content">
-      <NFTsHeader hasAccounts={hasAccounts} />
+      <NFTsHeader
+        hasAccounts={hasAccounts}
+        nfts={nftCounts}
+        collections={collectionCount}
+        badges={badgeCount}
+        totalInCurrency={someAmount}
+        totalInETH={someAmountInETH}
+        mainCurrencySign={mainCurrencySign}
+        loading={NFTsLoading}
+      />
 
       {/* TODO: Move these to their respective tab */}
       <NFTsExploreBanner type="nfts" />
